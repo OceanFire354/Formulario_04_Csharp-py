8485da8e9cad0d914f1a1b1f7497fc468641ef48
OceanFire354 <142519721+OceanFire354@users.noreply.github.com>
2023-10-02 21:32:05 -0600

    Add files via upload

---
+++ Formulario_Csharp/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario_Csharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombres = textBox1.Text;
+            string apellidos = textBox2.Text;
+            string telefono = textBox3.Text;
+            string estatura = textBox4.Text;
+            string edad = textBox5.Text;
+
+            string genero = "";
+            if (radioButton1.Checked)
+            {
+                genero = "Hombre";
+            }
+            else if (radioButton2.Checked)
+            {
+                genero = "Mujer";
+            }
+
+            string datos = ($"Nombre: {nombres}\r\nApellidos:{apellidos}\r\nTelefonos: {telefono}\r\nEstatura {estatura}:\r\nEdad: {edad}\r\nGenero: {genero}");
+
+            string rutaArchivo = ("C:\Users\leona\OneDrive\Documentos\Spider\Formulario-py.txt");
+
+            bool archivoExiste = File.Exists(rutaArchivo);
+            Console.WriteLine(archivoExiste);
+
+            if (archivoExiste == false)
+            {
+                File.WriteAllText(rutaArchivo, datos);
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(rutaArchivo))
+                {
+                    if (archivoExiste)
+                    {
+                        writer.WriteLine();
+                    }
+                    writer.WriteLine(datos);
+                }
+            }
+            MessageBox.Show($"Datos guardados con éxito\n\n{datos}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox4.Clear();
+            textBox3.Clear();
+            textBox5.Clear();
+        }
+    }
+}
